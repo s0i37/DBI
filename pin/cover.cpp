99f5adac062767a244d52d59330462b38300b7b8
s0i37 <s0i37@work>
2019-07-02 20:07:55 +0500

    added coverage assessment pin/cover

--- pin/cover.cpp
+++ pin/cover.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 #include <list>
 
-#define VERSION "0.38"
+#define VERSION "0.40"
+#define COVER_SIZE 0x1000
 
 #if defined(__i386__) || defined(_WIN32)
     #define HEX_FMT "0x%08x"
@@ -23,18 +24,36 @@ unsigned int max_bb_executed = 0;
 list <string> functions;
 unsigned int instructions = 0;
 unsigned int max_instructions = 0;
+unsigned char *cover = 0;
 
 KNOB<string> Knob_outfile(KNOB_MODE_WRITEONCE,  "pintool", "outfile", "cover.log", "Output file");
 KNOB<string> Knob_module(KNOB_MODE_WRITEONCE,  "pintool", "module", "", "tracing just this module");
 KNOB<unsigned int> Knob_max_bb(KNOB_MODE_WRITEONCE, "pintool", "max_bb", "0", "maximum count of basic blocks");
+
+void save_cover(void)
+{
+    unsigned int bb_count = 0;
+    if(!cover)
+        return;
+    for(unsigned int i = 0; i < COVER_SIZE; i++)
+        if(cover[i])
+            bb_count += 1;
+    fprintf(f, "[*] coverage %.02f%%\n", float(bb_count)*100/COVER_SIZE);
+    fflush(f);
+}
 
 VOID do_trace(ADDRINT eip)
 {
     fprintf(f, HEX_FMT "\n", eip-low_boundary);
     fflush(f);
     bb_executed++;
+    if(cover)
+        cover[ (eip-low_boundary) % COVER_SIZE ] = 0x01;
     if(bb_executed == max_bb_executed)
+    {
+        save_cover();
         PIN_Detach();
+    }
 }
 
 VOID trace_instrument(TRACE trace, VOID *v)
@@ -51,18 +70,31 @@ VOID trace_instrument(TRACE trace, VOID *v)
 
 VOID img_instrument(IMG img, VOID *v)
 {
+    SEC sec;
+    unsigned int code_size = 0;
     fprintf( f, "[*] module %s " HEX_FMT " " HEX_FMT "\n", IMG_Name(img).c_str(), IMG_LowAddress(img), IMG_HighAddress(img) );
     if(need_module != "" && strcasestr( IMG_Name(img).c_str(), need_module.c_str() ) )
     {
         fprintf( f, "[+] module instrumented: " HEX_FMT " " HEX_FMT " %s\n", IMG_LowAddress(img), IMG_HighAddress(img), IMG_Name(img).c_str() );
         low_boundary = IMG_LowAddress(img);
         high_boundary = IMG_HighAddress(img);
+
+        for(sec= IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec))
+            if ( SEC_IsExecutable(sec) )
+                if(SEC_Address(sec) != 0)
+                    code_size += SEC_Size(sec);
+
+        if(code_size)
+            cover = (unsigned char *)malloc(code_size);
+        if(cover)
+            memset(cover, 0, COVER_SIZE);
     }
     fflush(f);
 }
 
 VOID fini(INT32 code, VOID *v)
 {
+    save_cover();
     fflush(f);
     fclose(f);
 }
@@ -84,6 +116,7 @@ int main(int argc, char ** argv)
     need_module = Knob_module.Value();
     outfile_name = Knob_outfile.Value().c_str();
     max_bb_executed = Knob_max_bb.Value();
+
     f = fopen(outfile_name, "w");
 
     IMG_AddInstrumentFunction(img_instrument, 0);
