0cf02c73884125669bd5fe958886e83735ecd4f5
soier <soier@work>
2020-04-27 01:16:31 +0500

    added runtime statistics in pin/stats

--- pin/stats.cpp
+++ pin/stats.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-#define VERSION "0.12"
+#define VERSION "0.13"
 
 FILE * f;
 const char * outfile_name;
@@ -16,10 +16,22 @@ struct Module
 	ADDRINT high_addr;
 	string name;
 };
+struct Symbol
+{
+	unsigned int id;
+	unsigned int module_id;
+	ADDRINT low_addr;
+	ADDRINT high_addr;
+	string name;
+};
 unsigned int modules_loaded = 0;
+unsigned int symbols_available = 0;
 list <struct Module> modules;
+list <struct Symbol> symbols;
 map <unsigned int, unsigned int> modules_call;
 map <unsigned int, unsigned int> modules_exec;
+map <unsigned int, unsigned int> symbols_call;
+map <unsigned int, unsigned int> symbols_exec;
 unsigned int instructions = 0;
 unsigned int max_instructions = 0;
 
@@ -35,44 +47,137 @@ unsigned int get_module_id(ADDRINT addr)
 	return 0;
 }
 
-void save_stats()
+unsigned int get_symbol_id(ADDRINT addr)
 {
-	f = fopen(outfile_name, "w");
+	list <struct Symbol>::iterator it;
+	for( it = symbols.begin(); it != symbols.end(); it++ )
+		if( addr >= it->low_addr && addr <= it->high_addr )
+			return it->id;
+	return 0;
+}
+
+void modules_stats()
+{
 	list <struct Module>::iterator module;
-	fprintf(f, "module\tcalls\texec\n");
-	for( module = modules.begin(); module != modules.end(); module++ )
-		fprintf(f, "%s\t%u\t%u\n", module->name.c_str(), modules_call[module->id], modules_exec[module->id]);
-	fclose(f);
+	list <unsigned int> calls;
+
+	for(module = modules.begin(); module != modules.end(); module++)
+		calls.push_back(modules_call[module->id]);
+	calls.sort();
+
+	printf("module\tcalls\texec\n");
+	do
+	{
+		for( module = modules.begin(); module != modules.end(); module++ )
+		{
+			if(modules_call[module->id] == calls.back())
+			{
+				printf("0x%08lx \t (%u) %s \t %u \t %u\n",
+					module->low_addr, module->id, module->name.c_str(), modules_call[module->id], modules_exec[module->id]);
+				break;
+			}
+		}
+		calls.pop_back();
+	}
+	while(calls.size() != 0);
+
+	for(module = modules.begin(); module != modules.end(); module++)
+	{
+		modules_call[module->id] = 0;
+		modules_exec[module->id] = 0;
+	}	
+}
+
+void symbols_stats(unsigned int module_id)
+{
+	unsigned int limit = 20;
+	list <struct Symbol>::iterator symbol;
+	list <unsigned int> calls;
+	list <unsigned int> execs;
+
+	for(symbol = symbols.begin(); symbol != symbols.end(); symbol++)
+		if(module_id && symbol->module_id == module_id)
+			calls.push_back(symbols_call[symbol->id]);
+	calls.sort();
+
+	printf("symbol\tcalls\texec\n");
+	do
+	{
+		for(symbol = symbols.begin(); symbol != symbols.end(); symbol++)
+		{
+			if(module_id && symbol->module_id == module_id)
+			{
+				if(symbols_call[symbol->id] == calls.back())
+				{
+					printf("%s \t %u \t %u\n",
+						symbol->name.c_str(), symbols_call[symbol->id], symbols_exec[symbol->id]);
+					break;
+				}
+			}
+		}
+		calls.pop_back();
+		if(--limit == 0)
+			break;
+	}
+	while(calls.size() != 0);
+
+	for(symbol = symbols.begin(); symbol != symbols.end(); symbol++)
+	{
+		symbols_call[symbol->id] = 0;
+		symbols_exec[symbol->id] = 0;
+	}	
 }
 
 VOID do_exec(ADDRINT addr)
 {
 	unsigned int module_id;
+	//unsigned int symbol_id;
 	if( (module_id = get_module_id(addr)) != 0 )
 		modules_exec[module_id]++;
+	//if( (symbol_id = get_symbol_id(addr)) != 0 )
+	//	symbols_exec[symbol_id]++;
 
 	instructions += 1;
 	if(instructions == max_instructions)
 	{
-		save_stats();
+		modules_stats();
 		PIN_Detach();
+	}
+	if(instructions % 100000 == 0)
+	{	
+		modules_stats();
+		//symbols_stats(8);
 	}
 }
 
 VOID do_call(ADDRINT addr)
 {
 	unsigned int module_id;
+	unsigned int symbol_id;
 	if( (module_id = get_module_id(addr)) != 0 )
 		modules_call[module_id]++;
+	if( (symbol_id = get_symbol_id(addr)) != 0 )
+		symbols_call[symbol_id]++;
 }
 
 
 VOID img_instrument(IMG img, VOID * v)
 {
+	RTN rtn;
+	SEC sec;
 	struct Module module = { ++modules_loaded, IMG_LowAddress(img), IMG_HighAddress(img), IMG_Name(img) };
 	modules.push_front( module );
 	modules_call[modules_loaded] = 0;
 	modules_exec[modules_loaded] = 0;
+
+	for( sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec) )
+		for( rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn) )
+		{
+			RTN_Open(rtn);
+			struct Symbol symbol = { ++symbols_available, modules_loaded, RTN_Address(rtn), RTN_Address(rtn) + RTN_Range(rtn), RTN_Name(rtn).c_str() };
+			symbols.push_front(symbol);
+			RTN_Close(rtn);
+		}
 }
 
 VOID rtn_instrument(RTN rtn, VOID *v)
@@ -90,7 +195,7 @@ VOID ins_instrument(INS ins, VOID *v)
 
 VOID fini(INT32 code, VOID *v)
 {
-	save_stats();
+	modules_stats();
 }
 
 int main(int argc, char ** argv)
